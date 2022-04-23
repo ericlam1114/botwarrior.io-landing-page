767e4c2d73182a05e3bcffc5c7b63186bcb1fe8e
ericlam1114 <83894250+ericlam1114@users.noreply.github.com>
2022-04-23 09:03:12 -0700

    fixed alt tags

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,53 +19,53 @@ return (
 	</h1>
 	<Container>
 		<Row>
-	<a href="#" className="logo">
+	<a href="/" className="logo">
 
 <img src={logo} alt="" />
 
 </a>
 		<Column>
 			<Heading>Features</Heading>
-			<FooterLink href="#">Profitability</FooterLink>
-			<FooterLink href="#">Optimization</FooterLink>
-			<FooterLink href="#">Advantage</FooterLink>
+			<FooterLink href="/">Profitability</FooterLink>
+			<FooterLink href="/">Optimization</FooterLink>
+			<FooterLink href="/">Advantage</FooterLink>
 		</Column>
 		<Column>
 			<Heading>About</Heading>
-			<FooterLink href="#">Developed in 2022 by R.O. Ventures.</FooterLink>
-			{/* <FooterLink href="#">Internships</FooterLink>
-			<FooterLink href="#">Coding</FooterLink>
-			<FooterLink href="#">Teaching</FooterLink> */}
+			<FooterLink href="/">Developed in 2022 by R.O. Ventures.</FooterLink>
+			{/* <FooterLink href="/">Internships</FooterLink>
+			<FooterLink href="/">Coding</FooterLink>
+			<FooterLink href="/">Teaching</FooterLink> */}
 		</Column>
 		<Column>
 			<Heading>Waitlist</Heading>
-			<FooterLink href="#">Join Now</FooterLink>
+			<FooterLink href="/">Join Now</FooterLink>
 		
 		</Column>
 		<Column>
 			{/* <Heading>Social Media</Heading>
-			<FooterLink href="#">
+			<FooterLink href="/">
 			<i className="fab fa-facebook-f">
 				<span style={{ marginLeft: "10px" }}>
 				Facebook
 				</span>
 			</i>
 			</FooterLink>
-			<FooterLink href="#">
+			<FooterLink href="/">
 			<i className="fab fa-instagram">
 				<span style={{ marginLeft: "10px" }}>
 				Instagram
 				</span>
 			</i>
 			</FooterLink>
-			<FooterLink href="#">
+			<FooterLink href="/">
 			<i className="fab fa-twitter">
 				<span style={{ marginLeft: "10px" }}>
 				Twitter
 				</span>
 			</i>
 			</FooterLink>
-			<FooterLink href="#">
+			<FooterLink href="/">
 			<i className="fab fa-youtube">
 				<span style={{ marginLeft: "10px" }}>
 				Youtube
