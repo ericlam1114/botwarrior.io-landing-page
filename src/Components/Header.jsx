641359079f1c498d82b3705192f10fffd9ee6a50
ericlam1114 <83894250+ericlam1114@users.noreply.github.com>
2022-05-02 14:21:08 -0700

    site name update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -41,7 +41,9 @@ const Header = () => {
            <div>
             <div className="contact-input contact-center"> 
             <input  onChange={handleInputChange} type='email' value={email} onBlur={(e) => e.target.placeholder = 'example@gmail.com'} onFocus={(e) => e.target.placeholder = ''} placeholder='example@gmail.com' required /><a href="/"><button className="buttonStyle" >Join Now</button></a>
+
            </div></div></div><div className="shiftThis">
+            
                 <SideImage /></div>
             </div></div>
 </form>
